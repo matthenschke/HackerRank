e262ceebee24b5e3a3c2d50c1764fae024b4c167
Matthew Henschke <matthewhenschke98@gmail.com>
2018-08-29 19:57:38 -0400

    Create reverseArrary.java

---
+++ Data Structures/reverseArrary.java
@@ -0,0 +1,30 @@
+/*
+ *An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  (you may also see it written as ).
+
+Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.
+
+Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
+
+Input Format
+
+The first line contains an integer,  (the number of integers in ). 
+The second line contains  space-separated integers describing .
+
+Constraints
+
+Output Format
+
+Print all  integers in  in reverse order as a single line of space-separated integers.
+ */
+static int[] reverseArray(int[] a) {
+        int last = a.length - 1;
+        int i = 0;
+        while (i <= last){
+            int temp = a[i];
+            a[i] = a[last];
+            a[last] = temp;
+            i++;
+            last--;
+}
+        return a;
+    }
